a621ea16b9757e81d604f61bf7898312889c174e
bang666asik <kevinhumala012@gmail.com>
2025-01-05 04:00:00 +0000

    Setup WebSocket connections

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -32,10 +32,13 @@ const buttonVariants = cva(
       size: "default",
     },
   }
+)
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
